266f93017f23d1d043711dea3fa2910231ad2516
MiiZZo <miizzo.js@mail.ru>
2023-07-01 04:44:21 +0300

    build: fix generating d.ts files after build

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,18 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/main.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'effector-forms-zod',
       fileName: 'index',
     },
     rollupOptions: {
       external: ['effector', 'zod'],
+      output: {
+        globals: {
+          effector: 'effector',
+          zod: 'zod',
+        },
+      },
     },
   },
 })
